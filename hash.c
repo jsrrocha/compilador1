7929d3f716ea0e8260a1275c15c05ac97e94b6b1
Matheus <m.pereira96@hotmail.com>
2018-09-10 20:40:23 -0300

    Insert into hash

--- hash.c
+++ hash.c
@@ -58,7 +58,7 @@ void hashPrint(void){
     int i;
     for(i=0;i<HASH_SIZE;i++){
         for(node=Table[i]; node; node = node->next){
-            fprintf(stderr,"table[%d] has %s\n",i,node->text);
+            fprintf(stderr,"table[%d] has %s with type %d\n",i,node->text,node->type);
         }
     }
 }
